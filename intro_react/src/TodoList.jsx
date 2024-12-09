c900189d913b4075d869175e94bc264de7d18de7
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-12-09 17:39:23 +0100

    ex3 done

--- intro_react/src/TodoList.jsx
+++ intro_react/src/TodoList.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const Header = () => {
     return (
         <header>
@@ -19,14 +21,42 @@ const Button = () => {
 }
 
 const List = () => {
+    const handleCheck = (id) => {
+        setTodos(todos.map(todo => {
+            if (todo.id === id) {
+                return {
+                    ...todo,
+                    isCompleted: !todo.isCompleted
+                }
+            }
+            return todo;
+        }));
+    }
+    const initialTodos = [
+        {id: 1, text: "Learn React", isCompleted: false},
+        {id: 2, text: "Learn React again", isCompleted: false},
+        {id: 3, text: "Go home", isCompleted: false}
+    ];
+    
+    
+    const [todos, setTodos] = useState(initialTodos);
+
     return (
         <ul className="list">
-            <li> <input type="checkbox" /> Todo 1 </li>
-            <li> <input type="checkbox" /> Todo 2 </li>
-            <li> <input type="checkbox" /> Todo 3 </li>
+            {todos.map((todo, index) => {
+                return <li key={index}> 
+                <input 
+                id="{todo.id}" 
+                type="checkbox" 
+                name="{todo.name}" 
+                checked={todo.isCompleted}
+                onChange={() =>handleCheck(todo.id)} /> {todo.text}
+                </li>
+            })}
         </ul>
     )
 }
+
 
 
 export { Header, Input, Button, List }
