0e30996e9842bc3322bfa75422bbc5003fecb5e4
Sam Sepiol <94198807+sam-sepiol1@users.noreply.github.com>
2024-12-12 18:44:54 +0100

    exercise 2

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 export const Header = () => {
     return (
         <div className="header">
@@ -16,13 +18,21 @@ export const Input = () => {
 }
 
 export const TodoList = () => {
+    const initalTodos = [
+        { id: 1, text: "Todo 1", completed: false },
+        { id: 2, text: "Todo 2", completed: false },
+        { id: 3, text: "Todo 33333", completed: false },
+        { id: 4, text: "Todo 4", completed: false },
+    ]
+
+    const [todos, setTodos] = useState(initalTodos)
     return (
         <>
         <h2>Todo List</h2>
         <ul className="todo-list">
-            <li className="todo-list--item"><input type="checkbox"></input>Todo 1</li>
-            <li className="todo-list--item"><input type="checkbox"></input>Todo 2</li>
-            <li className="todo-list--item"><input type="checkbox"></input>Todo 3</li>
+            {todos.map((todo) => (
+                <li className="todo-list--item" key={todo.id}> <input type="checkbox" />{todo.text}</li>
+            ))}
         </ul>
         </>
     )
